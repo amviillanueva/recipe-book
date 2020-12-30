872076a81d0da8a2e8ac5251dbbb970eb66e75e4
amviillanueva <angelikam.villanueva@gmail.com>
2020-12-30 15:42:43 +0800

    Add style in about, edit recipe js

--- js/recipes.js
+++ js/recipes.js
@@ -1,5 +1,19 @@
-function dishDisplay(){
-    const displayDish = document.getElementById("display-dish");
-    displayDish.classList.remove("none");
-    console.log(displayDish)
+function dishDisplay(type){
+    const mainDish = document.getElementById("display-dish-main");
+    const sideDish = document.getElementById("display-dish-side");
+    const dessertDish = document.getElementById("display-dish-dessert");
+
+    if (type === 'main') {
+        mainDish.classList.remove("none");
+        sideDish.classList.add("none");
+        dessertDish.classList.add("none");
+    } else if (type === 'side') {
+        mainDish.classList.add("none");
+        sideDish.classList.remove("none");
+        dessertDish.classList.add("none");
+    } else if (type === 'dessert') {
+        mainDish.classList.add("none");
+        sideDish.classList.add("none");
+        dessertDish.classList.remove("none");
+    }
 }
